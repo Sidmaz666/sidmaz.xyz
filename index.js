e14324f953c7057973669034f91d7b5dff53d32e
sidmaz666 <sidmazumder8@gmail.com>
2024-01-16 12:59:44 +0530

    Use CORS   🚀

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(session({
   }),
   secret: process.env.SESSION_SECRET, 
   resave: true, 
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: { secure: true, sameSite: "none", maxAge: 24 * 60 * 60 * 1000 },
 }));
 
