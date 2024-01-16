cafe44e1b527bf9c7807cae002c62641747d1d1d
sidmaz666 <sidmazumder8@gmail.com>
2024-01-16 09:06:34 +0530

    Improve vercel support   🚀

--- functions/db_connect.js
+++ functions/db_connect.js
@@ -1,10 +1,32 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 require("dotenv").config()
 
-const connection = async () => {
-    const con = await mongoose.connect(process.env.MONGODB_URI)
-    console.log(`Connected Established!`)
-    return con
-}
+const connectToMongoDB = async () => {
+  const mongoDBUri = process.env.MONGODB_URI;
+  const maxRetries = 3;
+  let currentRetry = 0;
 
-module.exports = connection
+  const connectWithRetry = async () => {
+    try {
+      await mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error(`Error connecting to MongoDB: ${error.message}`);
+
+      if (currentRetry < maxRetries) {
+        currentRetry++;
+        const delay = 1000 * Math.pow(2, currentRetry); 
+        console.log(`Retrying in ${delay / 1000} seconds...`);
+        setTimeout(connectWithRetry, delay);
+      } else {
+        console.error('Max connection retries reached. Exiting application.');
+        process.exit(1);
+      }
+    }
+  };
+
+  await connectWithRetry();
+};
+
+
+module.exports = connectToMongoDB
