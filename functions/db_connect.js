eae85a7d266af0b4b22d3e19ed338f482e760f22
sidmaz666 <sidmazumder8@gmail.com>
2024-01-16 09:17:49 +0530

    Improve vercel support   🚀

--- functions/db_connect.js
+++ functions/db_connect.js
@@ -8,7 +8,7 @@ const connectToMongoDB = async () => {
 
   const connectWithRetry = async () => {
     try {
-      await mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true,  serverSelectionTimeoutMS: 30000 });
+      await mongoose.connect(mongoDBUri, { useNewUrlParser: true, serverSelectionTimeoutMS: 30000 });
       console.log('Connected to MongoDB');
     } catch (error) {
       console.error(`Error connecting to MongoDB: ${error.message}`);
