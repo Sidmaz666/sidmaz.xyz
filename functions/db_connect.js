e0c02063d283363ba7647799429593f6539a90e5
sidmaz666 <sidmazumder8@gmail.com>
2024-01-16 08:59:54 +0530

    Improve vercel support   🚀

--- functions/db_connect.js
+++ functions/db_connect.js
@@ -2,17 +2,9 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 const connection = async () => {
-  try {
     const con = await mongoose.connect(process.env.MONGODB_URI)
     console.log(`Connected Established!`)
     return con
-  } catch (error) {
-    console.log({error})
-    return {
-      error,
-      status: "error"
-    }
-  }
 }
 
 module.exports = connection
