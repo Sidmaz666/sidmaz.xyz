3e4e9ae0b66ac14763d9d9e560c99fc0d299f52c
sidmaz666 <sidmazumder8@gmail.com>
2024-01-17 09:27:10 +0530

    Redirect on Connection Failure!

--- functions/db_connect.js
+++ functions/db_connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = () => {
-    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect(process.env.MONGODB_URI);
     mongoose.connection
     .once('open', () => { 
         console.log('Connection to the DB established');
