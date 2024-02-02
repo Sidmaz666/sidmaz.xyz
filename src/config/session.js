55a5b345627c89aeff326016279e141d16efbb15
sidmaz666 <sidmazumder8@gmail.com>
2024-02-02 13:15:12 +0530

    Updated: Check  NODE_ENV Value

--- src/config/session.js
+++ src/config/session.js
@@ -2,7 +2,7 @@ const sessionOptions = {
   password:process.env.SESSION_SIGN,
   cookieName: "connect.sid",
   cookieOptions:{
-    secure:process.env.NODE_ENV === "pro"
+    secure:process.env.NODE_ENV === "production"
   }
 }
 
